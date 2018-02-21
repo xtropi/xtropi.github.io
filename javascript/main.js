a0aa0acfb0bd2929c259542ab5b6e76656461c1d
Mukhtar Musaev <m89821395289@yandex.ru>
2018-02-21 17:49:55 +0500

    m

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,32 @@
+function wait(ms){
+var d = new Date();
+var d2 = null;
+do { d2 = new Date(); }
+while(d2-d < ms);
+}
+
 window.onload = function(){
-	
-	$("#main")[0].onmousedown=function(){
-		alert("1");
+	main=$("#main")[0];
+	activity=$("#activity")[0];
+	bio=$("#bio")[0];
+	contacts=$("#contacts")[0];
+	maincontent=$(".mainContent")[0];
+	main.onmousedown=function(){
+		var i=0;
+		while(i<1){
+		maincontent.style.opacity=i;
+		i=i+0.01;
+		//wait(100);
+		}
 	};
-	$("#activity")[0].onmousedown=function(){
-		alert("2");
+	activity.onmousedown=function(){
+		
 	};
-	$("#bio")[0].onmousedown=function(){
-		alert("3");
+	bio.onmousedown=function(){
+		
 	};
-	$("#contacts")[0].onmousedown=function(){
-		alert("4");
+	contacts.onmousedown=function(){
+		maincontent.style.opacity=0;
 	};
 
 	
