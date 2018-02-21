e20bff9f399f860443bfe938575ef56e61c19d53
Mukhtar Musaev <m89821395289@yandex.ru>
2018-02-21 17:20:25 +0500

    js+jquery initial

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+	
+	$("#main")[0].onmousedown=function(){
+		alert("1");
+	};
+	$("#activity")[0].onmousedown=function(){
+		alert("2");
+	};
+	$("#bio")[0].onmousedown=function(){
+		alert("3");
+	};
+	$("#contacts")[0].onmousedown=function(){
+		alert("4");
+	};
+
+	
+	
+	
+};
