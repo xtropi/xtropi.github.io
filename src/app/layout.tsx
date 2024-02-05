4b9e61419d4800e05f3649f44a0d6bccc5412978
Mukhtar <mukh.musaev@gmail.com>
2024-02-05 23:31:49 +0500

    pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/solana-validator">Solana Validator</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/solana-validator/page.tsx
+++ src/app/solana-validator/page.tsx
@@ -1,12 +1,10 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
-      Section which introduces me as tech guy
+      Solana validator information such as location, hardware, software, total stake, APY
     </main>
   );
 }
