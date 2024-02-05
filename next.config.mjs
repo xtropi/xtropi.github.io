d1e0cd3065e341f923e4ba699a5db6b78caa974e
Mukhtar <mukh.musaev@gmail.com>
2024-02-05 23:51:40 +0500

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    reactStrictMode: true,
+    basePath: '/github-pages',
+    // output: 'export',
 };
 
 export default nextConfig;
