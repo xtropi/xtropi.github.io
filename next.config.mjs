d4d400728e6bb338caa486d4fb3fa15f4aed06a2
Mukhtar <mukh.musaev@gmail.com>
2024-02-06 00:46:30 +0500

    workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    distDir: 'dist',
     output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig
